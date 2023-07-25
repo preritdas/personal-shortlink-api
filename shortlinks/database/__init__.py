9da69ba0cf37de9b1135ebed145e4360d6e3c895
Prerit Das <preritdas@gmail.com>
2023-07-25 20:00:46 +0000

    Expose checks module.

--- shortlinks/database/__init__.py
+++ shortlinks/database/__init__.py
@@ -3,6 +3,9 @@ from pymongo.mongo_client import MongoClient
 
 from keys import KEYS
 
+# Expose
+from database import checks
+
 
 MONGO_CLIENT = MongoClient(KEYS.MongoDB.connect_str)
 DATABASE = MONGO_CLIENT["Shortlinks"]
