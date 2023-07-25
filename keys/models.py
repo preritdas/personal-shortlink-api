8baa2811e88d4bc99b2b7712146e3d128f21d44f
Prerit Das <preritdas@gmail.com>
2023-07-25 19:33:31 +0000

    Keys and Mongo.

---
+++ keys/models.py
@@ -0,0 +1,17 @@
+"""
+This module defines Pydantic models for validating a YAML configuration file containing 
+API keys and settings for various services such as OpenAI, Twilio, etc. Must define 
+a final Keys model class which has class variables for each service, and each service
+is a BaseModel class with the keys (and private settings) for that service.
+"""
+from pydantic import BaseModel
+
+
+class MongoDBModel(BaseModel):
+    """MongoDB connection credentials."""
+    connect_str: str
+
+
+class Keys(BaseModel):
+    """Overall keys."""
+    MongoDB: MongoDBModel
