13972af7697baaab89c207ceb7359ed5373bd0b9
Prerit Das <preritdas@gmail.com>
2023-07-25 20:16:15 +0000

    Include router and root redirector.

--- api/__init__.py
+++ api/__init__.py
@@ -4,13 +4,23 @@ from fastapi.responses import RedirectResponse
 
 from shortlinks import find_shortlink
 
+from api.manage import router
+
 
 app = FastAPI()
+
+# Include manage router
+app.include_router(router, prefix="/manage")
+
+
+@app.get("/")
+async def root_redirect():
+    """If the user visits the root path, redirect them to the main website."""
+    return RedirectResponse("https://preritdas.com")
 
 
 @app.get("/{code}")
 async def main_handler(code: str):
-    """If the user visits the root path, redirect them to the main website."""
     shortlink = find_shortlink(code)
 
     if not shortlink:
