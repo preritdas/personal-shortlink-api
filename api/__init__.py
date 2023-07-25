a9ce4961cbeb9daca21e72cfab0756d2683265f1
Prerit Das <preritdas@gmail.com>
2023-07-25 20:07:39 +0000

    Base main handler.

--- api/__init__.py
+++ api/__init__.py
@@ -2,11 +2,18 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+from shortlinks import find_shortlink
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root_redirect():
+@app.get("/{code}")
+async def main_handler(code: str):
     """If the user visits the root path, redirect them to the main website."""
-    return RedirectResponse("https://preritdas.com")
+    shortlink = find_shortlink(code)
+
+    if not shortlink:
+        return "Shortlink not found or shortlink expired."
+
+    return RedirectResponse(shortlink)
