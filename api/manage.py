ffa9774130f2937f0e99889b8d1d95b1730dd833
Prerit Das <preritdas@gmail.com>
2023-07-25 20:21:32 +0000

    Proper creation.

--- api/manage.py
+++ api/manage.py
@@ -1,6 +1,6 @@
 """CRUD operations for the links."""
 from fastapi import APIRouter
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
 from datetime import datetime
 
@@ -14,10 +14,18 @@ class Link(BaseModel):
     """Link model."""
     code: str
     url: str
-    expiration: datetime = None
+    expiration: str = Field("", description="Expiration date and time in YYYY-MM-DD format.")
+
+    @validator("expiration")
+    def expiration_datetime(cls, v):
+        """Validate expiration."""
+        if v:
+            return datetime.strptime(v, "%Y-%m-%d")
+
+        return v
 
 
-@router.get("/create")
+@router.post("/create")
 async def create(shortlink: Link):
     """Create a shortlink."""
     res = create_shortlink(shortlink.url, shortlink.code, shortlink.expiration)
