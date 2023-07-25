29885de7a0f1410dd5cdd87efaaf8e4f545bfc5c
Prerit Das <preritdas@gmail.com>
2023-07-25 19:26:33 +0000

    Root redirect endpoint.

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,12 @@
 """API routers."""
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root_redirect():
+    """If the user visits the root path, redirect them to the main website."""
+    return RedirectResponse("https://preritdas.com")
