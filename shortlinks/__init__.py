ad72f61a210c177ce917356bdcc6c863c983dd80
Prerit Das <preritdas@gmail.com>
2023-07-26 20:14:17 +0000

    Filter datetimes in search.

--- shortlinks/__init__.py
+++ shortlinks/__init__.py
@@ -5,26 +5,26 @@ from shortlinks.database import SHORTLINKS
 from shortlinks.database.checks import code_exists, code_expired
 
 
-def create_shortlink(url: str, code: str, expiration: datetime = None) -> bool:
-    """Create a shortlink. Returns False if the chosen code exists."""
-    if code_exists(code):
-        return False
-
-    SHORTLINKS.insert_one({"url": url, "code": code, "expiration": expiration})
-    return True
-
-
 def find_shortlink(code: str) -> str:
     """
     Find a shortlink. Returns the URL. If it doesn't exist or if it exists 
     but is expired, return an empty string.
     """
-    item = SHORTLINKS.find_one({"code": code})
+    item = SHORTLINKS.find_one({"code": code, "expiration": {"$gte": datetime.now()}})
 
-    if item and not code_expired(code):
+    if item:
         return item["url"]
-
+    
     return ""
+
+
+def create_shortlink(url: str, code: str, expiration: datetime = None) -> bool:
+    """Create a shortlink. Returns False if the chosen code exists."""
+    if find_shortlink(code):  # if a non-expired shortlink already exists
+        return False
+
+    SHORTLINKS.insert_one({"url": url, "code": code, "expiration": expiration})
+    return True
 
 
 def delete_shortlink(code: str) -> bool:
